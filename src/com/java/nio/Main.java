d05873e257e2e32b9074501bf8922fbc40e35eaa
luc-dev10 <luc.dev1008@gmail.com>
2022-01-15 23:03:38 -0500

    Add initial setup for application

---
+++ src/com/java/nio/Main.java
@@ -0,0 +1,13 @@
+package com.java.nio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // write your code here
+        System.out.println("Hello there");
+        System.out.println("Hello there");
+        // write your code here
+
+    }
+}
